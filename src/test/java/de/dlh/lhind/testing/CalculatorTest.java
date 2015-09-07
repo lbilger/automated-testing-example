959d4a4c0b8ca00b371a4263e1c7ba54ac630d8d
Lars Bilger <lars.bilger@lhind.dlh.de>
2015-09-07 23:01:46 +0200

    [Slide 19] Changed needle test to provide real parser instance using InjectionProvider.

--- src/test/java/de/dlh/lhind/testing/CalculatorTest.java
+++ src/test/java/de/dlh/lhind/testing/CalculatorTest.java
@@ -2,21 +2,18 @@ package de.dlh.lhind.testing;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.needle4j.annotation.InjectIntoMany;
 import org.needle4j.annotation.ObjectUnderTest;
 import org.needle4j.junit.NeedleRule;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.needle4j.injection.InjectionProviders.providerForInstance;
 
 public class CalculatorTest {
     @Rule
-    public NeedleRule needleRule = new NeedleRule();
+    public NeedleRule needleRule = new NeedleRule(providerForInstance(new CommaSeparatedParser()));
 
     @ObjectUnderTest
     private Calculator calculator;
-
-    @InjectIntoMany
-    private Parser parser = new CommaSeparatedParser();
 
     @Test
     public void returnsExpectedResultForInput() {
