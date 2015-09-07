26a835812ee9ab366b5320b720d5e935925e35c6
Lars Bilger <lars.bilger@lhind.dlh.de>
2015-09-07 21:59:35 +0200

    [Slide 12] Changed Mockito example to use doAnswer-when mocking style.

--- src/test/java/de/dlh/lhind/testing/CalculatorTest.java
+++ src/test/java/de/dlh/lhind/testing/CalculatorTest.java
@@ -1,27 +1,42 @@
 package de.dlh.lhind.testing;
 
+import com.google.common.base.Function;
+import com.google.common.base.Splitter;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.mockito.stubbing.Answer;
 
-import static java.util.Arrays.asList;
+import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doAnswer;
 
 public class CalculatorTest {
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
-    
+
     @Mock
     private Parser parser;
 
     @Test
     public void returnsExpectedResultForInput() {
         Calculator calculator = new Calculator(parser);
-        doReturn(asList(1, 3, 0)).when(parser).parse("1,3,0");
+        doAnswer(new Answer() {
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                return newArrayList(transform(Splitter.on(",").split((String) invocation.getArguments()[0]),
+                        new Function<String, Integer>() {
+                            public Integer apply(String input) {
+                                return Integer.valueOf(input);
+                            }
+                        }));
+            }
+        }).when(parser).parse(anyString());
 
         int result = calculator.calculate("1,3,0");
 
