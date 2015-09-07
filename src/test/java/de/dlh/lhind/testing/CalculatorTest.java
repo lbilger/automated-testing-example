8772482a52e3ff747f582a5dee75776c0ba1f3b6
Lars Bilger <lars.bilger@lhind.dlh.de>
2015-09-07 23:00:39 +0200

    [Slide 19] Changed needle test to use real parser instance.

--- src/test/java/de/dlh/lhind/testing/CalculatorTest.java
+++ src/test/java/de/dlh/lhind/testing/CalculatorTest.java
@@ -2,13 +2,11 @@ package de.dlh.lhind.testing;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.needle4j.annotation.InjectIntoMany;
 import org.needle4j.annotation.ObjectUnderTest;
 import org.needle4j.junit.NeedleRule;
 
-import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 public class CalculatorTest {
     @Rule
@@ -17,13 +15,11 @@ public class CalculatorTest {
     @ObjectUnderTest
     private Calculator calculator;
 
-    @Mock
-    private Parser parser;
+    @InjectIntoMany
+    private Parser parser = new CommaSeparatedParser();
 
     @Test
     public void returnsExpectedResultForInput() {
-        when(parser.parse("1,3,0")).thenReturn(asList(1, 3, 0));
-        
         int result = calculator.calculate("1,3,0");
 
         assertThat(result).isEqualTo(4);
