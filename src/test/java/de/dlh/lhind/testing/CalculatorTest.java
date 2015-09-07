e3747c1878d26f2acdc4ae2d6821b6891f94a858
Lars Bilger <lars.bilger@lhind.dlh.de>
2015-09-07 21:52:02 +0200

    [Slide 11] Changed Mockito example to use annotations.

--- src/test/java/de/dlh/lhind/testing/CalculatorTest.java
+++ src/test/java/de/dlh/lhind/testing/CalculatorTest.java
@@ -1,17 +1,21 @@
 package de.dlh.lhind.testing;
 
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CalculatorTest {
 
+    @Mock
+    private Parser parser;
+
     @Test
     public void returnsExpectedResultForInput() {
-        Parser parser = mock(Parser.class);
+        MockitoAnnotations.initMocks(this);
         Calculator calculator = new Calculator(parser);
         when(parser.parse("1,3,0")).thenReturn(asList(1, 3, 0));
 
